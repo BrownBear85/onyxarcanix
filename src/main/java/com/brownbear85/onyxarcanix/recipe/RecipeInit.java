a61547d5ebb86ae0f95632e86030027a8da737a1
BrownBear85 <91921122+BrownBear85@users.noreply.github.com>
2022-11-17 09:12:48 -0800

    altar jei texture

---
+++ src/main/java/com/brownbear85/onyxarcanix/recipe/RecipeInit.java
@@ -0,0 +1,19 @@
+package com.brownbear85.onyxarcanix.recipe;
+
+import com.brownbear85.onyxarcanix.OnyxArcanix;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class RecipeInit {
+    public static final DeferredRegister<RecipeSerializer<?>> SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, OnyxArcanix.MODID);
+
+    public static final RegistryObject<RecipeSerializer<AltarRecipe>> ALTAR_CRAFTING_SERIALIZER =
+            SERIALIZERS.register("altar_crafting", () -> AltarRecipe.Serializer.INSTANCE);
+
+    public static void register(IEventBus bus) {
+        SERIALIZERS.register(bus);
+    }
+}
