abb4785a3c357f425030a7d3086ffa87942b32e3
BrownBear85 <91921122+BrownBear85@users.noreply.github.com>
2022-11-18 19:26:14 -0800

    magic particle

--- src/main/java/com/brownbear85/onyxarcanix/init/ParticleInit.java
+++ src/main/java/com/brownbear85/onyxarcanix/init/ParticleInit.java
@@ -1,6 +1,8 @@
 package com.brownbear85.onyxarcanix.init;
 
 import com.brownbear85.onyxarcanix.OnyxArcanix;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.particles.DustParticleOptions;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -12,6 +14,13 @@ public class ParticleInit {
     public static final DeferredRegister<ParticleType<?>> PARTICLE_TYPES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, OnyxArcanix.MODID);
 
     public static final RegistryObject<SimpleParticleType> ALTAR_PARTICLES = PARTICLE_TYPES.register("altar_particles", () -> new SimpleParticleType(true));
+
+    public static final RegistryObject<ParticleType<DustParticleOptions>> MAGIC_PARTICLE = PARTICLE_TYPES.register("magic_particle", () -> new ParticleType<>(true, DustParticleOptions.DESERIALIZER) {
+        @Override
+        public Codec<DustParticleOptions> codec() {
+            return DustParticleOptions.CODEC;
+        }
+    });
 
 
     public static void register(IEventBus bus) {
